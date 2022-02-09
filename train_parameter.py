a8ff96343606de557e0057c8c489a744b04530db
费政聪 <3300949577@qq.com>
2022-02-09 21:42:11 +0800

    Add files via upload

--- train_parameter.py
+++ train_parameter.py
@@ -1,24 +1,33 @@
 import torch 
 from model import ParameterProject 
-from utils import parameter_dict_combine, weight_dict_print, weight_size_dict_generate
-
+from utils import parameter_dict_combine, weight_dict_print, weight_size_dict_generate, weight_resize_for_model_load
+from model import vgg11_bn  
 
 def train(): 
 
+    # 1. download ckpt 
     ckpt_path_list = ['ckpt/vgg11_bn.pth', 'ckpt/vgg11_bn.pth'] 
     weight_dict_list = [] 
-
     for ckpt_path in ckpt_path_list:
         weight_dict_list.append(torch.load(ckpt_path, map_location=None)) 
-    
     # weight_dict_print(weight_dict_list[0])
     combine_weight_dict, weight_size_dict = parameter_dict_combine(weight_dict_list)
     # weight_dict_print(combine_weight_dict)
     combine_weight_size_dict = weight_size_dict_generate(combine_weight_dict)
     
-    model = ParameterProject(weight_size_dict=combine_weight_size_dict) 
-    loss = model(combine_weight_dict)
-    print(loss)
+
+    # 2. model parameter predict 
+    parameter_predict_model = ParameterProject(weight_size_dict=combine_weight_size_dict) 
+    generated_weight_dict = parameter_predict_model(combine_weight_dict)
+    generated_weight_dict = weight_resize_for_model_load(generated_weight_dict, weight_dict_list[0]) 
+    # weight_dict_print(generated_weight_dict) 
+    
+    # 3. model parameter load
+    vgg_model = vgg11_bn() 
+    vgg_model.load_state_dict(generated_weight_dict) 
+    
+    # 4. training 
+
 
 if __name__ == '__main__': 
     train() 
--- utils.py
+++ utils.py
@@ -7,7 +7,6 @@ import torch
 from train_vgg import OUTPUT_DIM 
 
 
-
 def data_plot():
     cifar10_path = 'data/cifar-10/data_batch_1' 
     with open(cifar10_path, 'rb') as f: 
@@ -28,10 +27,11 @@ def ckpt_load(ckpt_path):
     final_fc = nn.Linear(IN_FEATURES, OUTPUT_DIM) 
     model.classifier[-1] = final_fc 
 
-    print(model) 
-    print(model.state_dict().keys())
-    for key in model.state_dict().keys():
-        print(key, model.state_dict()[key].size()) 
+    #print(model) 
+    #print(model.state_dict().keys())
+    #for key in model.state_dict().keys():
+    #    print(key, model.state_dict()[key].size()) 
+    return model 
 
 
 def weight_dict_print(weight_dict): 
@@ -70,6 +70,17 @@ def parameter_dict_combine(weight_dict_list):
         combine_weight_dict[key] = weight_matrix 
     return combine_weight_dict, weight_size_dict 
  
+
+ # resize the weight dict for model loading 
+def weight_resize_for_model_load(weight_dict, original_weight_dict): 
+    for key in original_weight_dict.keys(): 
+        if key in weight_dict.keys():
+            weight_dict[key] = weight_dict[key].view(original_weight_dict[key].size())
+        else:
+            weight_dict[key] = original_weight_dict[key]
+    return weight_dict
+    
+
 
 
 if __name__ == '__main__': 
